511e3d174bdf2e87e2d19dff604dca407d3c6b73
ubuntu <shota.oda@bizreach.co.jp>
2016-07-19 11:11:06 +0900

    commit

---
+++ src/main/scala/Array/Test.scala
@@ -0,0 +1,8 @@
+package Array
+
+/**
+  * Created by shota.oda on 2016/07/15.
+  */
+class Test {
+
+}
---
+++ src/main/scala/polymorphism/Element.scala
@@ -0,0 +1,8 @@
+package polymorphism
+
+/**
+  * Created by shota.oda on 2016/07/15.
+  */
+class Element {
+
+}
