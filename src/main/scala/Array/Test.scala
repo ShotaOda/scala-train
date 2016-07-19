067b834f2977e24652289d5c852ab5687d35d788
ubuntu <shota.oda@bizreach.co.jp>
2016-07-19 11:21:28 +0900

    delete 重複

--- src/main/scala/Array/Test.scala
+++
@@ -1,8 +0,0 @@
-package Array
-
-/**
-  * Created by shota.oda on 2016/07/15.
-  */
-class Test {
-
-}
--- src/main/scala/polymorphism/Element.scala
+++
@@ -1,8 +0,0 @@
-package polymorphism
-
-/**
-  * Created by shota.oda on 2016/07/15.
-  */
-class Element {
-
-}
