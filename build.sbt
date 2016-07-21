e740d5aff7b775e799c8520380357bf6d0377ae3
ubuntu <shota.oda@bizreach.co.jp>
2016-07-21 11:26:50 +0900

    ready for akka

--- build.sbt
+++ build.sbt
@@ -4,3 +4,8 @@ version := "1.0"
 
 scalaVersion := "2.11.8"
 
+//scalaのバージョンなどについては%%表記すれば設定値から読み込み
+libraryDependencies ++= Seq(
+  "com.typesafe.akka" %% "akka-actor" % "2.4.8"
+)
+
---
+++ src/main/scala/actor/Runner.scala
@@ -0,0 +1,12 @@
+package actor
+
+import akka.actor.Actor
+import akka.actor.ActorSystem
+import akka.actor.Props
+
+/**
+  * Created by shota.oda on 2016/07/20.
+  */
+object Runner extends App {
+
+}
